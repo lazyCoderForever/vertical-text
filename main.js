f2e1897a974cfe8e61e3db991d79d26c0ee1318d
Roman <roman.kasumov98@mail.ru>
2020-10-23 15:46:40 +0300

    Vertical text app

---
+++ main.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+  function checkPressedButton(e) {
+    if (/[a-z]/.test(e.key)) {
+      return true
+    }
+    return false
+  }
+
+  function addNewSymbol(checked, pressedButton) {
+    const mainFrame = document.querySelector('.main-frame')
+    const line = document.querySelector('.line')
+    const newSymbol = document.createElement('p')
+    newSymbol.classList.add('symbol')
+
+    if (checked(pressedButton)) {
+      line.remove()
+      newSymbol.innerText = pressedButton.key
+      mainFrame.appendChild(newSymbol)
+      mainFrame.appendChild(line)
+      return true
+    } else {
+      console.log('Не допустимый символ')
+    }
+  }
+
+  document.addEventListener('keydown', (e) =>
+    addNewSymbol(checkPressedButton, e)
+  )
+})
